b4605f3d59c2ac1a771274cec63a516c1e0d9cc9
Kamil <tomiczek.kamil26@gmail.com>
2023-06-15 12:23:48 +0200

    oncheck change apperance

--- ToDo/MainPage.xaml.cs
+++ ToDo/MainPage.xaml.cs
@@ -2,10 +2,12 @@
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
+    Color checkBoxColor;
+
+    public MainPage()
 	{
 		InitializeComponent();
-	}
+    }
 
     private void ItemTapped(object sender, EventArgs e)
     {
@@ -15,6 +17,30 @@ public partial class MainPage : ContentPage
         if (check != null)
         {
             ((CheckBox)check).IsChecked = !((CheckBox)check).IsChecked;
+        }
+    }
+
+    private void CheckBoxCheckedChange(object sender, CheckedChangedEventArgs e)
+    {
+        var checkBox = sender as CheckBox;
+        checkBox.Color = Color.FromArgb("#666");
+
+        var parent = checkBox.Parent as Layout;
+
+        var label = parent.FirstOrDefault(c => c.GetType() == typeof(Label)) as Label;
+
+        if (checkBox.IsChecked)
+        {
+            label.TextDecorations = TextDecorations.Strikethrough;
+            label.TextColor = Color.FromArgb("#666");
+            parent.Background = Color.FromArgb("#c8c7c9");
+            checkBox.Color = Color.FromArgb("#512BD4");
+        }
+        else
+        {
+            label.TextDecorations = TextDecorations.None;
+            label.TextColor = Color.FromArgb("#000");
+            parent.Background = Color.FromArgb("#e7defc");
         }
     }
 }
