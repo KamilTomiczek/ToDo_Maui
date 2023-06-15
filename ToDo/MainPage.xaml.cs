161932303c681fcd5c5d8ce8aef04b47038b31b3
Kamil <tomiczek.kamil26@gmail.com>
2023-06-15 11:56:43 +0200

    appearance

---
+++ ToDo/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace ToDo;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void ItemTapped(object sender, EventArgs e)
+    {
+        var layout = sender as Layout;
+        var check = layout.FirstOrDefault(c => c.GetType() == typeof(CheckBox));
+
+        if (check != null)
+        {
+            ((CheckBox)check).IsChecked = !((CheckBox)check).IsChecked;
+        }
+    }
+}
+
